c7333791b84981f5d717609453453d949e0645f4
jakubmraz <jakub.mraz111@gmail.com>
2023-04-27 12:53:49 +0200

    heh

--- Scripts/ScriptRunner.py
+++ Scripts/ScriptRunner.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 # Replace this with the name of your existing script
 script_name = 'Segmentator.py'
+script_name2 = "FeatureDetector.py"
 
 # Supported image file extensions
 image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.ico')
@@ -19,6 +20,7 @@ def main():
         if file.is_file() and file.suffix.lower() in image_extensions:
             # Run your existing script with the image path as an argument
             subprocess.run([sys.executable, script_name, str(file)])
+            subprocess.run([sys.executable, script_name2, str(file)])
 
 if __name__ == '__main__':
     main()
